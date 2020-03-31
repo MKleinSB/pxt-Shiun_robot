9c0c9e12c80fce00d2462b8322726cccf30510cf
Michael Klein <37311672+MKleinSB@users.noreply.github.com>
2020-03-31 21:47:45 +0200

    first release

--- test.ts
+++ test.ts
@@ -1,1 +1,5 @@
 // Hier kann man Tests durchführen; diese Datei wird nicht kompiliert, wenn dieses Paket als Erweiterung verwendet wird.
+basic.forever(function () {
+    MiniPOW.set_offset(AnalogPin.P0, 0, AnalogPin.P1, 0, AnalogPin.P2, 0)
+    MiniPOW.motion(MiniPOW.dnum.Stop)
+})
